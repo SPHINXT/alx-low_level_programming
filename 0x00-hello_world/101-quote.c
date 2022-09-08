8210c70e62135f9cdd3cd228def01c44e9d1e93a
SPHINXT <tobiasbakre@gmail.com>
2022-09-09 01:55:55 +0530

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,10 @@
 #include <unistd.h>
 /**
  * main - main value
- * Return: 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
