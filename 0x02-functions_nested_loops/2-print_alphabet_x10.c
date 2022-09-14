437be4a914f3a5c8241413cb698fceece11e0ca6
SPHINXT <tobiasbakre@gmail.com>
2022-09-14 06:26:33 +0530

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * Return - 0
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
+	char ch;
 	int i;
 
-	for (i = 0, i < 10, i++)
-	{
-		char ch;
+	i = 0;
 
-		for (ch = 'a', ch <= 'z', ch++)
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
