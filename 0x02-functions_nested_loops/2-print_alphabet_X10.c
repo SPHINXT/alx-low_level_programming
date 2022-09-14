851c1e46464870c23ca6ed6975d879f5c01f77c8
SPHINXT <tobiasbakre@gmail.com>
2022-09-14 06:10:26 +0530

    Update 2-print_alphabet_X10.c

--- 0x02-functions_nested_loops/2-print_alphabet_X10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_X10.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet 10 times
- * Return: void
+ * print_alphabet - Entry point
+ * Return:0
  */
-void print_alphabet_x10(void)
+
+void print_alphabet(void)
 {
-	int i;
+	char c = 'a';
 
-	for ( i = 0, i < 10, i++)
+	while (c <= 'z')
 	{
-		char ch;
+		_putchar(c);
+		c++;
 
-		for ( ch = 'a', ch <= 'z', ch++)
-		{
-			_putchar(ch);
-			_putchar('\n');
-		}
 	}
-}	
+	_putchar('\n');
+
+}
