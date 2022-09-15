1ed647e891d74456a4db9b56ed9df9e024611685
Oluwatobi <tobiasbakre@gmail.com>
2022-09-15 13:29:34 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
