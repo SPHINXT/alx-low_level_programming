480812a37f98aefe0a01d93a4e003034f095101b
Oluwatobi <tobiasbakre@gmail.com>
2022-09-15 08:46:05 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Function to check for an uppercase alphabet
+ * Result: 1 is success
+ */
+
+int _isupper(int c)
+{
+	if(c >= 'A' & c <= 'Z')
+	return (1);
+}
