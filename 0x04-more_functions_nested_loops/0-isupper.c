623051588787af953ff00f316c5f01c5bec394dc
SPHINXT <tobiasbakre@gmail.com>
2022-09-15 21:24:24 +0530

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - Function to check for an uppercase alphabet
- *@c: the character to be checked
- * Return: 1 is success
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' & c <= 'Z')
-	return (1);
+	return (c >= 'A' && c <= 'Z');
 }
