b99be5052e995f7b1c8f65f8880a3d781da81114
SPHINXT <tobiasbakre@gmail.com>
2022-09-15 21:21:49 +0530

    @c - character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 
 /**
  * _isupper - Function to check for an uppercase alphabet
- * Result: 1 is success
+ *@c: the character to be checked
+ * Return: 1 is success
  */
 
 int _isupper(int c)
 {
-	if(c >= 'A' & c <= 'Z')
+	if (c >= 'A' & c <= 'Z')
 	return (1);
 }
