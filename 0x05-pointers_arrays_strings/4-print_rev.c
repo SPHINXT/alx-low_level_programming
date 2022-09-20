ea9d9b4b90e90484af1104153af4b764233ccc5a
SPHINXT <tobiasbakre@gmail.com>
2022-09-20 21:35:18 +0100

    task 4!

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,12 @@
  */
 void print_rev(char *s)
 {
-	if (*s != '\0')
 	{
-	print_rev(s + 1);
-	_putchar(*s);
+		if (*s != '\0')
+		{
+		print_rev(s + 1);
+		_putchar(*s);
+		}
 	}
-
 	_putchar('\n');
 }
