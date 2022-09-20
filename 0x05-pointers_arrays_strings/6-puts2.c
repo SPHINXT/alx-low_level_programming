8d72c77d4e3c09887f6bea3796a726a4e846da86
SPHINXT <tobiasbakre@gmail.com>
2022-09-20 22:11:58 +0100

    task 6!

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,19 +7,10 @@
  */
 void puts2(char *str)
 {
-	int len, i;
-
-	len = 0;
-
-	while (str[len] != '\0')
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		len++;
+		_putchar(str{i});
 	}
-
-	for (i = 0; i < len; i += 2)
-	{
-		_putchar(str[i]);
-	}
-
-	_putchar('\n');
 }
