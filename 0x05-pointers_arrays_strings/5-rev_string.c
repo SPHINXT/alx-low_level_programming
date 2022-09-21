66d23f8c85aabb5866dde8735cbfce0e568a4525
SPHINXT <tobiasbakre@gmail.com>
2022-09-21 01:31:28 +0100

    task 5!

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,17 @@
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
-
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
+	int length, i, middle;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	length = i;
+	middle = length / 2;
+	char temp;
+	
+	for (i = 0; i < middle; i++)
 	{
-		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
 	}
 }
