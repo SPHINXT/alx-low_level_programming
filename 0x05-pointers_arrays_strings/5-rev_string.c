ff087c2c21de54da8c3e9daf45326b73988acfc1
SPHINXT <tobiasbakre@gmail.com>
2022-09-21 01:48:19 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@ void rev_string(char *s)
 {
 	int length, i, middle, len1;
 	char temp;
-	
+
 	for (length = 0; s[length] != '\0'; i++)
 	middle = length / 2;
 	len1 = length - 1;
-	
+
 	for (i = 0; i < middle; i++)
 	{
 		temp = s[i];
-		s[i] = s[len1 - i];
+		s[i] = s[len1];
 		s[len1--] = temp;
 	}
 }
