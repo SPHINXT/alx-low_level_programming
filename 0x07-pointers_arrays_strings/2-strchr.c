ec888c2e8b5678b4abf685372893faa5475b5ba1
Oluwatobi <tobiasbakre@gmail.com>
2022-09-28 05:38:09 -0700

    fixed bug

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,17 +12,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	
+
 	for (i = 0; s[i] = '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s);
 		}
-		break;
-	}
-
-	else 
+		else
 	{
 		return (NULL);
 	}
