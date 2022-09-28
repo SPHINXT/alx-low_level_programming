3441ab1c37e8fc14368dd3a2747ebd25051b5ddb
Oluwatobi <tobiasbakre@gmail.com>
2022-09-28 07:54:00 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @s: param 1
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if ( *s == '\0')
+	{ 
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
