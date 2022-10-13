0adeca0a2aaad6494d2081f5effa0fe4f557dcd7
SPHINXT <tobiasbakre@gmail.com>
2022-10-13 22:27:29 +0530

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,30 +1,28 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print numbers arguments
- * @separator: string in between
- * @n: numbers of arguments
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list tob;
 	unsigned int i;
+	va_list list;
 
-	va_start(tob, n);
+	va_start(list, n);
+
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(tob, int));
-
-		if (i != (n - 1))
-		{
-			printf("%s", separator);
-		}
-		if (i == (n - 1))
-			printf("\n");
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
-	va_end(tob) ;
+	va_end(list);
+
+	printf("\n");
 }
